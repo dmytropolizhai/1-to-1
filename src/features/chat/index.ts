9c8012be8d0ff215002b9952b134fe86eda8c695
Dmytro Polizhai <dmytropolizhai@gmail.com>
2026-03-05 13:18:56 +0200

    feat: Create index file for export

---
+++ src/features/chat/index.ts
@@ -0,0 +1,4 @@
+export { MessageChat } from "./components/message-chat";
+export { MessageInput } from "./components/message-input";
+export { MessageItem as Message } from "./components/message-item";
+export type { MessageProps } from "./components/message-item";
